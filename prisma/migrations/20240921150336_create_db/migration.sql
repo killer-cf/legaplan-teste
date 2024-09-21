6fe9166f6fdf3ae33675b3df7d898dabe88bfa80
Kilder Costa <costa.kilder@live.com>
2024-09-21 12:31:32 -0300

    config: add prisma

---
+++ prisma/migrations/20240921150336_create_db/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "finished" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
