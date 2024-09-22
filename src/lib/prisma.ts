4247ec545b53201fe8d9f2b9c96d6660ab22bd37
Kilder Costa <costa.kilder@live.com>
2024-09-22 01:04:19 -0300

    refactor: prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from '@prisma/client'
 
 declare global {
-	var prisma: PrismaClient | undefined
+	var client: PrismaClient | undefined
 }
 
-export const client = globalThis.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = client
+export const prisma = globalThis.client || new PrismaClient()
+if (process.env.NODE_ENV !== 'production') globalThis.client = prisma
